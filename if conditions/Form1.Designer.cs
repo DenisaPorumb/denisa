3b1cbf4b45ab5f12c465721a71962768097b4bac
CRISTINA-AURA-MARIA BUȚEA <cristina.butea@stud.ubbcluj.ro>
2024-05-09 10:39:55 +0300

    Add project files.

---
+++ if conditions/Form1.Designer.cs
@@ -0,0 +1,118 @@
+﻿namespace if_conditions
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.youare = new System.Windows.Forms.TextBox();
+            this.lookfor = new System.Windows.Forms.TextBox();
+            this.okbtn = new System.Windows.Forms.Button();
+            this.cancelbtn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(241, 64);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(66, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "you are==";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(241, 121);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(131, 16);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "you are looking for==";
+            // 
+            // youare
+            // 
+            this.youare.Location = new System.Drawing.Point(322, 57);
+            this.youare.Name = "youare";
+            this.youare.Size = new System.Drawing.Size(234, 22);
+            this.youare.TabIndex = 2;
+            // 
+            // lookfor
+            // 
+            this.lookfor.Location = new System.Drawing.Point(378, 121);
+            this.lookfor.Name = "lookfor";
+            this.lookfor.Size = new System.Drawing.Size(219, 22);
+            this.lookfor.TabIndex = 3;
+            // 
+            // okbtn
+            // 
+            this.okbtn.Location = new System.Drawing.Point(258, 291);
+            this.okbtn.Name = "okbtn";
+            this.okbtn.Size = new System.Drawing.Size(75, 23);
+            this.okbtn.TabIndex = 4;
+            this.okbtn.Text = "ok";
+            this.okbtn.UseVisualStyleBackColor = true;
+            this.okbtn.Click += new System.EventHandler(this.okbtn_Click);
+            // 
+            // cancelbtn
+            // 
+            this.cancelbtn.Location = new System.Drawing.Point(453, 291);
+            this.cancelbtn.Name = "cancelbtn";
+            this.cancelbtn.Size = new System.Drawing.Size(75, 23);
+            this.cancelbtn.TabIndex = 5;
+            this.cancelbtn.Text = "cancel";
+            this.cancelbtn.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.cancelbtn);
+            this.Controls.Add(this.okbtn);
+            this.Controls.Add(this.lookfor);
+            this.Controls.Add(this.youare);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox youare;
+        private System.Windows.Forms.TextBox lookfor;
+        private System.Windows.Forms.Button okbtn;
+        private System.Windows.Forms.Button cancelbtn;
+    }
+}
+
